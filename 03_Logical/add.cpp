01272dfb2a02236a9ab94179f370b5922c7036c9
Sarisa Phulakorn <sarisa.beam@mail.kmutt.ac.th>
2021-11-16 23:59:59 +0700

    and

---
+++ 03_Logical/add.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  
+  int hunger = true;
+  int anger = true;
+  
+  if (hunger == true && anger == true) {
+ 
+  std::cout << "Hangry\n";
+ 
+}
+if (hunger && anger) {
+ 
+  std::cout << "Hangry\n";
+ 
+}
+  
+}
